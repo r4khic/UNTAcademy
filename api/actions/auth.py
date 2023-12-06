944cce1bb930f139269c6bee48c884917512a911
r4khic <r.arinov@kazdream.kz>
2023-12-06 15:21:51 +0600

    ref: moved action func to auth actions

--- api/actions/auth.py
+++ api/actions/auth.py
@@ -1,9 +1,17 @@
 from typing import Union
 
+from fastapi import Depends
+from fastapi import HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt
+from jose import JWTError
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
 
+import settings
 from db.dao import UserDAO
 from db.models import User
+from db.session import get_db
 from hashing import Hasher
 
 
@@ -21,4 +29,26 @@ async def authenticate_user(email: str, password: str, session: AsyncSession) ->
         return
     if not Hasher.verify_password(password, user.hashed_password):
         return
+    return user
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login/token")
+
+
+async def get_current_user_from_token(token: str = Depends(oauth2_scheme), session: AsyncSession = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+    )
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = await get_user_by_email_for_auth(email=email, session=session)
+    if user is None:
+        raise credentials_exception
     return user
--- api/login_handler.py
+++ api/login_handler.py
@@ -1,18 +1,14 @@
 from datetime import timedelta
-from typing import Union
 
 from fastapi import APIRouter
 from fastapi import Depends
 from fastapi import HTTPException
 from fastapi import status
-from fastapi.security import OAuth2PasswordBearer
 from fastapi.security import OAuth2PasswordRequestForm
-from jose import jwt
-from jose import JWTError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 import settings
-from api.actions.auth import get_user_by_email_for_auth, authenticate_user
+from api.actions.auth import authenticate_user
 from api.models import Token
 from db.session import get_db
 from security import create_access_token
@@ -35,26 +31,3 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
         expires_delta=access_token_expires,
     )
     return {"access_token": access_token, "token_type": "bearer"}
-
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login/token")
-
-
-async def get_current_user_from_token(token: str = Depends(oauth2_scheme), session: AsyncSession = Depends(get_db)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-    )
-    try:
-        payload = jwt.decode(
-            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
-        )
-        email: str = payload.get("sub")
-        if email is None:
-            raise credentials_exception
-    except JWTError:
-        raise credentials_exception
-    user = await get_user_by_email_for_auth(email=email, session=session)
-    if user is None:
-        raise credentials_exception
-    return user
