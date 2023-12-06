3c6340853abe91e914f1e94b422e33bda8076249
r4khic <r.arinov@kazdream.kz>
2023-12-06 15:35:17 +0600

    added jwt tokens access for endpoint user

--- api/handlers.py
+++ api/handlers.py
@@ -1,14 +1,13 @@
-from typing import Union
 from uuid import UUID
 
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from api.actions.auth import get_current_user_from_token
 from api.actions.user import create_new_user, delete_existing_user, get_existing_user_by_id, update_user
 from api.models import UserCreate, ShowUser, DeleteUserResponse, UpdatedUserResponse, UpdateUserRequest
-from db.dao import UserDAO
+from db.models import User
 from db.session import get_db
-from hashing import Hasher
 
 user_router = APIRouter()
 
@@ -19,7 +18,8 @@ async def create_user(body: UserCreate, db: AsyncSession = Depends(get_db)) -> S
 
 
 @user_router.delete("/", response_model=DeleteUserResponse)
-async def delete_user(user_id: UUID, db: AsyncSession = Depends(get_db)) -> DeleteUserResponse:
+async def delete_user(user_id: UUID, db: AsyncSession = Depends(get_db),
+                      current_user: User = Depends(get_current_user_from_token), ) -> DeleteUserResponse:
     deleted_user_id = await delete_existing_user(user_id, db)
     if deleted_user_id is None:
         raise HTTPException(status_code=404, detail=f"User with id {user_id} not found.")
@@ -27,7 +27,8 @@ async def delete_user(user_id: UUID, db: AsyncSession = Depends(get_db)) -> Dele
 
 
 @user_router.get("/", response_model=ShowUser)
-async def get_user_by_id(user_id: UUID, db: AsyncSession = Depends(get_db)) -> ShowUser:
+async def get_user_by_id(user_id: UUID, db: AsyncSession = Depends(get_db),
+                         current_user: User = Depends(get_current_user_from_token), ) -> ShowUser:
     user = await get_existing_user_by_id(user_id, db)
     if user is None:
         raise HTTPException(status_code=404, detail=f"User with id {user_id} not found.")
@@ -36,7 +37,8 @@ async def get_user_by_id(user_id: UUID, db: AsyncSession = Depends(get_db)) -> S
 
 @user_router.patch("/", response_model=UpdatedUserResponse)
 async def update_user_by_id(
-        user_id: UUID, body: UpdateUserRequest, db: AsyncSession = Depends(get_db)) -> UpdatedUserResponse:
+        user_id: UUID, body: UpdateUserRequest, db: AsyncSession = Depends(get_db),
+        current_user: User = Depends(get_current_user_from_token), ) -> UpdatedUserResponse:
     updated_user_params = body.dict(exclude_none=True)
     if updated_user_params == {}:
         raise HTTPException(status_code=422, detail="At least one parameter for user update info should be provided")
